da5031966b817f958bca407e5680dc5630848f6b
Thomas Schmitz <info@tschmitz.de>
2020-05-29 22:53:25 +0200

    Let bookmark service register at service discovery

--- bookmark-data-rest-service/src/test/java/de/tschmitz/rest/bookmarks/BookmarkRestServiceTest.java
+++ bookmark-data-rest-service/src/test/java/de/tschmitz/rest/bookmarks/BookmarkRestServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.hateoas.server.core.TypeReferences.EntityModelType;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.net.URI;
 
@@ -27,6 +28,7 @@ import static org.springframework.hateoas.MediaTypes.HAL_JSON;
  * web application context (e.g. WebEnvironment.RANDOM_PORT).
  */
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("dev")
 class BookmarkRestServiceTest {
 
     @Autowired
