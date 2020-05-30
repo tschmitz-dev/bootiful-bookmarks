4285dac00e02363fe9988cb300c35736c9789284
Thomas Schmitz <dev@tschmitz.de>
2020-05-30 18:38:36 +0200

    Add more test data, especially with second user_id

--- bookmark-data-rest-service/src/main/resources/data-h2.sql
+++ bookmark-data-rest-service/src/main/resources/data-h2.sql
@@ -8,13 +8,15 @@
 insert into bookmark (title,href,user_id) values('Spring Initializr','https://start.spring.io/','demo');
 insert into bookmark (title,href,user_id) values('Maven','https://maven.apache.org/guides/index.html','demo');
 insert into bookmark (title,href,user_id) values('Spring Maven Plugin','https://docs.spring.io/spring-boot/docs/2.2.7.RELEASE/maven-plugin/','demo');
-insert into bookmark (title,href,user_id) values('Baeldung | Java, Spring and Web Development tutorials','https://www.baeldung.com/','demo2');
+insert into bookmark (title,href,user_id) values('Baeldung | Java, Spring and Web Development tutorials','https://www.baeldung.com/','demo');
+
+insert into bookmark (title,href,user_id) values('Spring Data REST Security','https://docs.spring.io/spring-data/rest/docs/current/reference/html/#security','user');
+insert into bookmark (title,href,user_id) values('Spring Initializr','https://start.spring.io/','user');
+insert into bookmark (title,href,user_id) values('Maven','https://maven.apache.org/guides/index.html','user');
+insert into bookmark (title,href,user_id) values('Spring Framework Testing','https://docs.spring.io/spring-framework/docs/current/spring-framework-reference/testing.html','user');
 
 insert into tag (title) values ('Spring Framework');
 insert into tag (title) values ('Maven');
 
-insert into bookmarks_tags (bookmark_id,tag_id) values (select id from bookmark where title = 'Spring Initializr',select id from tag where title = 'Spring Framework');
-insert into bookmarks_tags (bookmark_id,tag_id) values (select id from bookmark where title = 'Maven',select id from tag where title = 'Maven');
-insert into bookmarks_tags (bookmark_id,tag_id) values (select id from bookmark where title = 'Spring Maven Plugin',select id from tag where title = 'Maven');
-insert into bookmarks_tags (bookmark_id,tag_id) values (select id from bookmark where title = 'Spring Maven Plugin',select id from tag where title = 'Spring Framework');
-insert into bookmarks_tags (bookmark_id,tag_id) values (select id from bookmark where title like 'Baeldung%',select id from tag where title = 'Spring Framework');
+insert into bookmarks_tags (bookmark_id, tag_id) select id, (select id from tag where title = 'Spring Framework') from bookmark where title like 'Spring%';
+insert into bookmarks_tags (bookmark_id, tag_id) select id, (select id from tag where title = 'Maven') from bookmark where title like 'Maven%';
