3fc661cf463ef97ef2dbaecc875b596316fa9ee5
Thomas Schmitz <dev@tschmitz.de>
2020-05-30 15:23:01 +0200

    Extend from root Repository interface
    This way we have more control over the REST api and its protection with
    spring security.

--- bookmark-data-rest-service/src/main/java/de/tschmitz/rest/bookmarks/BookmarkRepository.java
+++ bookmark-data-rest-service/src/main/java/de/tschmitz/rest/bookmarks/BookmarkRepository.java
@@ -1,6 +1,10 @@
 package de.tschmitz.rest.bookmarks;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.Repository;
+
+import java.util.Optional;
 
 /**
  * Data repository to perform CRUD operations on bookmark table.
@@ -8,5 +12,60 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * As long as we have spring-boot-starter-data-rest as dependency the Spring Data REST framework
  * will expose a discoverable REST API for bookmarks on top of this repository.
  */
-public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+public interface BookmarkRepository extends Repository<Bookmark, Long> {
+
+    /**
+     * Saves a given bookmark.
+     *
+     * @param bookmark must not be {@literal null}.
+     * @return the saved bookmark; will never be {@literal null}.
+     * @throws IllegalArgumentException in case the given {@literal bookmark} is {@literal null}.
+     */
+    Bookmark save(Bookmark bookmark);
+
+    /**
+     * Retrieves a bookmark by its id.
+     *
+     * @param id must not be {@literal null}.
+     * @return the bookmark with the given id or {@literal Optional#empty()} if none found.
+     * @throws IllegalArgumentException if {@literal id} is {@literal null}.
+     */
+    Optional<Bookmark> findById(Long id);
+
+    /**
+     * Returns a {@link Page} of bookmarks meeting the paging restriction provided in the {@code Pageable} object.
+     *
+     * @param pageable must not be {@literal null}.
+     * @return a page of entities
+     */
+    Page<Bookmark> findAll(Pageable pageable);
+
+    /**
+     * Deletes the bookmark with the given id.
+     *
+     * @param id must not be {@literal null}.
+     * @throws IllegalArgumentException in case the given {@literal id} is {@literal null}
+     */
+    void deleteById(Long id);
+
+    /**
+     * Deletes a given bookmark.
+     *
+     * @param bookmark must not be {@literal null}.
+     * @throws IllegalArgumentException in case the given bookmark is {@literal null}.
+     */
+    void delete(Bookmark bookmark);
+
+    /**
+     * Deletes the given bookmarks.
+     *
+     * @param bookmarks must not be {@literal null}. Must not contain {@literal null} elements.
+     * @throws IllegalArgumentException in case the given {@literal bookmarks} or one of its bookmarks is {@literal null}.
+     */
+    void deleteAll(Iterable<Bookmark> bookmarks);
+
+    /**
+     * Deletes all bookmarks.
+     */
+    void deleteAll();
 }
--- bookmark-data-rest-service/src/main/java/de/tschmitz/rest/bookmarks/TagRepository.java
+++ bookmark-data-rest-service/src/main/java/de/tschmitz/rest/bookmarks/TagRepository.java
@@ -1,14 +1,72 @@
 package de.tschmitz.rest.bookmarks;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.Repository;
+
+import java.util.Optional;
 
 /**
- * Data repository to perform CRUD operations on bookmark table.
+ * Data repository to perform CRUD operations on tag table.
  * <p>
  * As long as we have spring-boot-starter-data-rest as dependency the Spring Data REST framework
- * will expose a discoverable REST API for bookmarks on top of this repository.
+ * will expose a discoverable REST API for tags on top of this repository.
  */
-@Repository
-public interface TagRepository extends JpaRepository<Tag, Long> {
+public interface TagRepository extends Repository<Tag, Long> {
+
+    /**
+     * Saves a given tag.
+     *
+     * @param tag must not be {@literal null}.
+     * @return the saved tag; will never be {@literal null}.
+     * @throws IllegalArgumentException in case the given {@literal tag} is {@literal null}.
+     */
+    Tag save(Tag tag);
+
+    /**
+     * Retrieves a tag by its id.
+     *
+     * @param id must not be {@literal null}.
+     * @return the tag with the given id or {@literal Optional#empty()} if none found.
+     * @throws IllegalArgumentException if {@literal id} is {@literal null}.
+     */
+    Optional<Tag> findById(Long id);
+
+    /**
+     * Returns a {@link Page} of tags meeting the paging restriction provided in the {@code Pageable} object.
+     *
+     * @param pageable must not be {@literal null}.
+     * @return a page of entities
+     */
+    Page<Tag> findAll(Pageable pageable);
+
+    /**
+     * Deletes the tag with the given id.
+     *
+     * @param id must not be {@literal null}.
+     * @throws IllegalArgumentException in case the given {@literal id} is {@literal null}
+     */
+    void deleteById(Long id);
+
+    /**
+     * Deletes a given tag.
+     *
+     * @param tag must not be {@literal null}.
+     * @throws IllegalArgumentException in case the given tag is {@literal null}.
+     */
+    void delete(Tag tag);
+
+    /**
+     * Deletes the given tags.
+     *
+     * @param tags must not be {@literal null}. Must not contain {@literal null} elements.
+     * @throws IllegalArgumentException in case the given {@literal tags} or one of its tags is {@literal null}.
+     */
+    void deleteAll(Iterable<Tag> tags);
+
+    /**
+     * Deletes all tags.
+     */
+    void deleteAll();
 }
